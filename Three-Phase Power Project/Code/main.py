c95dcdf2f6c2ea22d5263ed85bc79a9d0d612391
Tim Doan <tdoan1213@gmail.com>
2026-06-23 23:20:45 -0700

    Added a prompt for user input and initial voltage explanation.

--- Three-Phase Power Project/Code/main.py
+++ Three-Phase Power Project/Code/main.py
@@ -4,4 +4,19 @@
 
 # Libraries
 
-# 
+# Introduction:
+print("Welcome to the Three-Phase Power Analyzer! This program will help you understand the basics of three-phase power systems and their applications.")
+
+# Prompt User for Input:
+user_input = input("To understand how widely used three-phase power is in everyday life, enter an input voltage: ")
+
+# Intial Explanation:
+print("For an input of " + user_input + "V, voltage travels as a sinusoidal wave. In three-phase power, three-voltages of " + user_input + "V are separated by a 120 degree phase difference. \n"
+"So, the three sinusoidal AC Voltages are:\n"
+"Va = " + user_input + "∠0⁰ V\n"
+"Vb = " + user_input + "∠120⁰ V\n"
+"Vc = " + user_input + "∠240⁰ V\n")
+
+# Calculations:
+
+# Explanations:
